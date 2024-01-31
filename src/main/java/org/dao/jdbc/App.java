007f6af53d1e1937f1335d2013fe4ace97269eae
weniosantana <wenio.souza123@gmail.com>
2024-01-31 15:11:00 -0300

    Adicionando o Projeto

---
+++ src/main/java/org/dao/jdbc/App.java
@@ -0,0 +1,13 @@
+package org.dao.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
