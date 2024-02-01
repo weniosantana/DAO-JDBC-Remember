f6937e4b322106f90ef37768a803b31c87777dd7
weniosantana <wenio.souza123@gmail.com>
2024-02-01 10:43:51 -0300

    Adicionando DaoFactory e SellerDaoJDBC

---
+++ src/main/java/org/dao/jdbc/model/dao/DaoFactory.java
@@ -0,0 +1,12 @@
+package org.dao.jdbc.model.dao;
+
+import org.dao.jdbc.model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+
+    }
+
+}
---
+++ src/main/java/org/dao/jdbc/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,36 @@
+package org.dao.jdbc.model.dao.impl;
+
+import org.dao.jdbc.model.dao.SellerDao;
+import org.dao.jdbc.model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+
+
+    @Override
+    public void insert(Seller obj) {
+
+    }
+
+    @Override
+    public void update(Seller obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
